2390d8e7101c91b04b4aa9fcaae47a7775cbc91f
mhdzumair <mhdzumair@gmail.com>
2019-06-26 05:26:29 +0000

    init

---
+++ e4-vendor-blobs.mk
@@ -0,0 +1,24 @@
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/moto/e4/vendor/bin,system/vendor/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/moto/e4/vendor/lib,system/vendor/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/moto/e4/vendor/lib64,system/vendor/lib64)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/moto/e4/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/moto/e4/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/moto/e4/lib64,system/lib64)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/moto/e4/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/moto/e4/vendor/etc,system/vendor/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/moto/e4/vendor/firmware,system/vendor/firmware)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/moto/e4/usr,system/usr)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/moto/e4/xbin,system/xbin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/moto/e4/thh,system/thh)
